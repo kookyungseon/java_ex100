83563048d491d75ec538755c719dd1b3414c0af3
kookyungseon <114800024+kookyungseon@users.noreply.github.com>
2023-11-05 00:11:39 +0900

    ex13 change

--- eclipse-workspace/java__ex100/src/java__ex100/ex100_13.java
+++ eclipse-workspace/java__ex100/src/java__ex100/ex100_13.java
@@ -1,1 +1,12 @@
+package java__ex100;
+import java.util.Scanner;
 
+public class ex100_13 {
+
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		float a = sc.nextFloat();
+		System.out.printf("%.2f", a);
+	}
+}
