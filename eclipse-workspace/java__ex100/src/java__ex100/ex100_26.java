c47fe51305abaadde71bd464bd32cdc9e35bab6f
kookyungseon <114800024+kookyungseon@users.noreply.github.com>
2023-11-20 14:10:35 +0900

    ex26

---
+++ eclipse-workspace/java__ex100/src/java__ex100/ex100_26.java
@@ -0,0 +1,16 @@
+package java__ex100;
+import java.util.*;
+public class ex100_26 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		String num = sc.nextLine();
+		
+		if(num.equals("0")) System.out.println(true);
+		else System.out.println(false);
+
+	}
+
+}
+
